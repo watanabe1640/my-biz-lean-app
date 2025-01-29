236e3a149e4d3651b139d71380a0f8b57e07ad38
watanabe1640 <taiki6554@icloud.com>
2025-01-29 18:04:37 +0900

    home更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <main className="flex-grow container mx-auto px-4 py-8">
         <div className="max-w-3xl mx-auto text-center">
           <h1 className="text-4xl font-bold mb-4">Tomarigi Quest</h1>
+          <h1 className="text-3xl font-bold mb-4">大人に最高の学びを</h1>
           <div className="grid gap-4 md:grid-cols-2">
             <Link
               href="/auth/login"
