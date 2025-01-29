4af1cba37407655e6066f21f2396096f633b3681
watanabe1640 <taiki6554@icloud.com>
2025-01-29 10:48:03 +0900

    テキストが見にくい箇所を修正

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "@/components/auth/LoginForm";
 // src/app/auth/login/page.tsx
 export default function LoginPage() {
 	return (
-	  <div className="min-h-screen bg-gray-50 flex flex-col justify-center">
+	  <div className="min-h-screen bg-gray-50 flex flex-col justify-center text-gray-900">
 		<div className="max-w-md w-full mx-auto">
 		  <div className="text-center mb-8">
 			<h2 className="text-3xl font-extrabold">Login</h2>
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from '@/components/auth/RegisterForm';
 
 export default function RegisterPage() {
  return (
-   <div className="min-h-screen bg-gray-50 flex flex-col justify-center">
+   <div className="min-h-screen bg-gray-50 flex flex-col justify-center text-gray-900">
      <div className="max-w-md w-full mx-auto">
        <h2 className="text-3xl font-extrabold text-center mb-8">Register</h2>
        <RegisterForm />
